9a55a0a100a54e5d06088cb3e9afe8f73272a8d1
a100750 <duarte.soares.araujo@gmail.com>
2025-07-29 00:36:34 +0100

    again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   ...compat.config({
     rules: {
       "react/no-unescaped-entities": "off",
-      "react/no-unused-vars": "off",
+      "no-unused-vars": "off",
     }}),
 ];
 
